f7b2bd66ec7dcf62f78c988fdcf432b1da39303a
Ivan-Mol <abyss.devotee@gmail.com>
2021-12-25 17:06:18 +0300

    Practice - Runners

---
+++ src/main/java/lesson4/ArraysMethods.java
@@ -0,0 +1,20 @@
+package lesson4;
+
+public class ArraysMethods {
+    public static <A> void replacingElementsInArray(A[] a1,int first,int second){
+        //меняет местами первое и второе
+        if (second>a1.length){
+            System.out.println("Массив не такой длинный");
+        }
+        if (first==second){
+            System.out.println("Первое и второе число одинаковые");
+        }
+        else {
+            A[] array = a1;
+            Object firstData = array[first];
+            array[first] = array[second];
+            array[second] = (A)firstData;
+            a1 = array;
+        }
+    }
+}
---
+++ src/main/java/lesson4/MainApp.java
@@ -0,0 +1,16 @@
+package lesson4;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String[] strings = {"a","ds","ads","dsa","ssdaa"};
+        Integer[] integers = {1,3,5,2};
+        ArraysMethods.replacingElementsInArray(integers,0,2);
+        System.out.println(Arrays.toString(integers));
+    }
+
+
+}
