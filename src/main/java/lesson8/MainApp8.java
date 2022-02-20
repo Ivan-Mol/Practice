6b417d4cb7ff91496a911e5f412a8e6ab076d164
Ivan-Mol <abyss.devotee@gmail.com>
2022-02-20 20:26:59 +0300

    Practice - Runners

---
+++ src/main/java/lesson8/MainApp8.java
@@ -0,0 +1,15 @@
+package lesson8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class MainApp8 {
+    public static void main(String[] args) {
+        //Создайте массив с набором слов, и с помощью Stream API найдите наиболее часто
+        //встречающееся;
+        String[] strings = {"один", "два", "два", "три", "четыре", "пять", "шесть"};
+
+    }
+}
