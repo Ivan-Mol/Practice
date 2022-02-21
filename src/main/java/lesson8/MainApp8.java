a7b79842b5ce1081e66668f8e416b2897c98fa6a
Ivan-Mol <abyss.devotee@gmail.com>
2022-02-21 13:20:55 +0300

    Practice - Runners

--- src/main/java/lesson8/MainApp8.java
+++ src/main/java/lesson8/MainApp8.java
@@ -1,8 +1,10 @@
 package lesson8;
 
+
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MainApp8 {
@@ -10,6 +12,9 @@ public class MainApp8 {
         //Создайте массив с набором слов, и с помощью Stream API найдите наиболее часто
         //встречающееся;
         String[] strings = {"один", "два", "два", "три", "четыре", "пять", "шесть"};
+        Map<String,Long> map = Arrays.stream(strings).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        String res = map.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("nothing");
+        System.out.println(res);
 
     }
 }
